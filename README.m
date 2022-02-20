8ab097c5acc7afc8f7530bbdf10e92c77c70a9c2
tes24 <100057040+tes24@users.noreply.github.com>
2022-02-20 12:52:13 +0800

    Create README.m

---
+++ README.m
@@ -0,0 +1,11 @@
+%% README
+
+% Codes related to Karkali et al Developmental Cell 2022
+% Contact: 
+% Sham Tlili <krystalysham@gmail.com>
+% Timothy Saunders <timothy.saunders@warwick.ac.uk>
+% Jose Munoz <j.munoz@upc.edu> 
+% if you have questions. Test data sets are provided.
+% Test sets should be unzipped prior to use.
+% Provides code for FEM, optic flow and generating 
+% kymographs as shown in paper.
